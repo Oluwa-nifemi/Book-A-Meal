59a469754b6da9d6d2ab86b772ad0d4b6c435601
Nifemi <deyeminifemi@gmail.com>
2019-02-22 10:13:45 +0100

    Created test for add to menu

--- test/menu-test.js
+++ test/menu-test.js
@@ -3,12 +3,13 @@ import request from 'chai-http';
 import app from '../index';
 import fs from 'fs';
 import path from 'path'; 
+import Menu from '../models/menu';
 
 const { expect, use } = chai;
 
 use(request);
 
-const p = path.join(__dirname, '../data', 'meals.json');
+const p = path.join(__dirname, '../data', 'menu.json');
 
 const apiVersion = '/api/v1';
 
@@ -20,6 +21,25 @@ describe('Get Menu', () => {
         .then(menu => {
             expect(menu).to.be.an('object');
             expect(menu).to.have.all.keys('date','meals')
+        })
+    })
+})
+
+describe('Add to Menu', () => {
+    it('Should return meal',() => {
+        chai.request(app)
+        .post(`${apiVersion}/menu`)
+        .send({
+            "id" : 5,
+            "quantity" : 7,
+        })
+        .then(res => res.body)
+        .then(meal => {
+            expect(meal).to.be.an('object');
+            expect(meal).to.have.all.keys('id','quantity');
+            const menu = Menu.getMenu();
+            menu.meals = menu.meals.filter(m => m.id !== meal.id);
+            fs.writeFileSync(p,JSON.stringify(menu));
         })
     })
 })
