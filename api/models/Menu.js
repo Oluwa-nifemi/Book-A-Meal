c41223b18cac76f0e720cecbd6e0db1b1a111ae2
Nifemi <deyeminifemi@gmail.com>
2019-03-01 20:34:58 +0100

    Added default value for date

--- api/models/Menu.js
+++ api/models/Menu.js
@@ -2,8 +2,13 @@ import Sequelize from 'sequelize';
 import db from '../config/database';
 
 const Menu = db.define('Menu', {
-    date: Sequelize.DATEONLY,
-    meals: Sequelize.ARRAY(Sequelize.TEXT),
+    date: {
+        type: Sequelize.DATEONLY,
+        defaultValue: Sequelize.NOW,
+    },
+    meals: {
+        type: Sequelize.ARRAY(Sequelize.TEXT),
+    },
 });
 
 export default Menu;
