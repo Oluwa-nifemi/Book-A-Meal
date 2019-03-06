cc3c88ecfd98ca4f75a940d973315d917a6b3860
Nifemi <deyeminifemi@gmail.com>
2019-03-06 04:50:27 +0100

    Added linebreak rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     "extends": "airbnb-base",
     rules : {
         "indent": ["error", 4],
+        "linebreak-style": ["error", "windows"]
     },
     "env": {
         "node": true
-    }
+    },
 };
