5edce25523e844db146e7dfeb3c07334a277ece3
Nifemi <deyeminifemi@gmail.com>
2019-03-06 14:39:29 +0100

    Removed duplicate code

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -6,17 +6,13 @@ dotenv.config();
 
 class User {
     static async login(req, res) {
-        const userdb = await UserModel.findOne({ where: { email: req.body.email } })
-        if (userdb) {
-            if (userdb.password === req.body.password) {
-                const token = jwt.sign({ id: userdb.id }, process.env.SECRET_KEY);
-                res.header('x-auth-token', token).status(200).send({
-                    status: 'success',
-                });
-                return true;
-            }
-            res.status(400).send('Invalid email or password');
-            return false;
+        const userdb = await UserModel.findOne({ where: { email: req.body.email } });
+        if (userdb && userdb.password === req.body.password) {
+            const token = jwt.sign({ id: userdb.id }, process.env.SECRET_KEY);
+            res.header('x-auth-token', token).status(200).send({
+                status: 'success',
+            });
+            return true;
         }
         res.status(400).send('Invalid email or password');
         return false;
